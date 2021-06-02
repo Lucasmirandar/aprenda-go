ddd7961c53639ba2c8f0fa776881d3be7d08294e
Lucas Miranda <lucasram14@gmail.com>
2021-06-02 08:40:46 -0300

    Create cap5exercicio5.go

---
+++ nivel2/cap5exercicio5.go
@@ -0,0 +1,13 @@
+/*- Crie uma variável de tipo string utilizando uma raw string literal.
+- Demonstre-a.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	x := `		 raw
+		string
+		literal`
+	fmt.Println(x)
+}
