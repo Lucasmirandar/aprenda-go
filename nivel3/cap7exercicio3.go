fc0cc42b3b0b35df5df737d417c5cc0669a3bea3
Lucas Miranda <lucasram14@gmail.com>
2021-06-07 19:00:47 -0300

    Create cap7exercicio3.go

---
+++ nivel3/cap7exercicio3.go
@@ -0,0 +1,12 @@
+//- Crie um loop utilizando a sintaxe: for condition {}
+//- Utilize-o para demonstrar os anos desde que você nasceu.
+
+package main
+
+import "fmt"
+
+func main() {
+	for ano := 1997; ano <= 2021; ano++ {
+		fmt.Println(ano)
+	}
+}
