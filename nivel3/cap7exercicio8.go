c37d1ce0c428f39261918ed31db4145c3bfc308e
Lucas Miranda <lucasram14@gmail.com>
2021-06-08 08:51:01 -0300

    Update cap7exercicio8.go

--- nivel3/cap7exercicio8.go
+++ nivel3/cap7exercicio8.go
@@ -1,1 +1,19 @@
 // - Crie um programa que utilize a declaração switch, sem switch statement especificado.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	switch {
+	case x < 10:
+		fmt.Println("X é menor que 10")
+	case x == 10:
+		fmt.Println("X é igual a 10")
+	case x > 10:
+		fmt.Println("X é maior que 10")
+	default:
+		fmt.Println("Erro")
+	}
+}
